a0dff945a43842c018969c73420a283e88498d4e
linux_china <libing.chen@gmail.com>
2021-07-30 15:36:55 -0700

    Add static handler & index page

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,31 @@ use axum::prelude::*;
 use std::net::SocketAddr;
 use http::StatusCode;
 use axum::response::{Html, Json};
+use axum::{routing::nest, service::ServiceExt};
 use serde_json::{json, Value};
+use tower_http::{services::ServeDir};
 
 #[tokio::main]
 async fn main() {
-    let app = route("/", get(root))
+    // static assets handler
+    let static_handle = ServeDir::new("./static").handle_error(|error: std::io::Error| {
+        Ok::<_, std::convert::Infallible>((StatusCode::INTERNAL_SERVER_ERROR, format!("Unhandled internal error: {}", error)))
+    });
+
+    let app = nest("/", axum::service::get(static_handle))
+        .route("/", get(index))
         .route("/html", get(html))
         .route("/json", get(json));
 
-    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
-    hyper::Server::bind(&addr)
+    let address = SocketAddr::from(([0, 0, 0, 0], 3000));
+    hyper::Server::bind(&address)
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-async fn root() -> &'static str {
-    "Hello, World!"
+async fn index() -> Html<&'static str> {
+    Html(include_str!("../static/index.html"))
 }
 
 // `Html` gives a content-type of `text/html`
